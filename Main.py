bb306429e15c73fcef79d860ea18f28a1b1e155f
Sealgram <liam.seagram@greenwwoodcollege.com>
2019-01-18 14:22:55 -0500

    Bot can now respond to basic messages, track the current user's name, and read the weather of specific cities to you.

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-import time
 import random
 import os
 import webbrowser as wb
 import speech_recognition as sr
 import subprocess
+import requests
 from tkinter import *
 
 
@@ -25,10 +25,33 @@ def recognize():
 
 def greeting():
     os.system("espeak 'Greetings! My name is Adolin'")
-    time.sleep(0.5)
     os.system("espeak 'I am a virtual assistant you can ask to do a variety of things.'")
-    time.sleep(0.5)
     os.system("espeak 'Click the 'ask' button to give me a command'")
+
+
+def generalresponses(saying):
+    names = open('names.txt', 'r')
+    namedoc = names.readlines()
+    try:
+        name = namedoc[0]
+    except IndexError:
+        name = ''
+    if saying == 'hello' or saying == 'hi':
+        os.system(f"espeak 'Hello {name}'")
+    elif saying == 'what is your favorite color':
+        os.system(f"espeak 'I don't think your eyes can perceive it, but it is beautiful, {name}")
+    elif saying == 'why is the sky blue':
+        os.system(f"espeak 'because that is the way you perceive it, {name}")
+    elif saying == 'do you like me':
+        os.system(f"espeak 'of course I like you, {name}, on a professional basis")
+
+
+def namerecord():
+    os.system("espeak 'What is your name'")
+    recordname = recognize()
+    names = open('names.txt', 'w+')
+    names.write(recordname)
+    os.system("espeak 'Your name has been recorded.'")
 
 
 def website():
@@ -62,14 +85,40 @@ def program():
     subprocess.Popen(["/usr/bin/open", "-W", "-n", "-a", str(pathsread[programchoice].strip())])
 
 
+def weather():
+    api_key = "89ea3f323d661f91d8df1df3388a2163"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    os.system("espeak 'Please tell me the name of the city you would like to get the weather for.'")
+    city_name = recognize()
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    if x["cod"] != "404":
+        y = x["main"]
+        current_temperature = y["temp"]
+        celsius = current_temperature - 273.15
+        z = x["weather"]
+        weather_description = z[0]["description"]
+        os.system(
+            f"espeak 'The temperature is {celsius:.2f} degrees celsius, and It is currently "
+            f"{weather_description} in {city_name}'"
+        )
+
+
 def functions():
     answer = recognize()
     searchweb = ['search web', 'web search', 'web', 'search', 'google', 'search the web']
     openprogram = ['open program', 'open', 'program open', 'open a program', 'open an app', 'app open']
+    weathercalls = ['weather', 'what is the weather', 'tell me the weather', 'find weather', 'whats the weather']
+    generalsayings = ['hello', 'hi', 'what is your favorite color', 'why is the sky blue', 'do you like me']
     if str(answer.lower()) in searchweb:
         website()
     elif str(answer.lower()) in openprogram:
         program()
+    elif str(answer.lower()) in weathercalls:
+        weather()
+    elif str(answer.lower()) in generalsayings:
+        generalresponses(answer)
 
 
 def generalresponse():
@@ -147,6 +196,8 @@ def maininterface():
             self.configure(background="gray33")
             quitbutton = Button(self, text="Quit", height=2, width=5, command=self.client_exit)
             quitbutton.place(x=230, y=400)
+            namebutton = Button(self, text="Name", height=2, width=5, command=self.name)
+            namebutton.place(x=230, y=350)
             askbutton = Button(self, text="Ask", height=8, width=30, command=self.ask)
             askbutton.place(x=125, y=100)
             informationbutton = Button(self, text="Click for Possible Commands", height=3, width=30, command=self.info)
@@ -161,11 +212,14 @@ def maininterface():
         def info(self):
             infopanel()
 
+        def name(self):
+            namerecord()
+
     root = Tk()
     root.geometry("500x500")
     app = Window2(root)
     root.mainloop()
 
 
-#startinterface()
+# startinterface()
 maininterface()
--- Test.py
+++ Test.py
@@ -1,2 +1,12 @@
-import subprocess
-subprocess.Popen(["/usr/bin/open", "-W", "-n", "-a", "/Applications/VLC.app"])
+import requests
+api_key = "89ea3f323d661f91d8df1df3388a2163"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
+city_name = input("Enter city name : ")
+complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+response = requests.get(complete_url)
+x = response.json()
+if x["cod"] != "404":
+    y = x["main"]
+    current_temperature = y["temp"]
+    celsius = current_temperature - 273.15
+    print(f"The temperature is {celsius:.2f} degrees celsius")
